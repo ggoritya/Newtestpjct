4de96667aa37ad0fdd6da0cf5926f0848c5cbdcd
ggoritya <158r1a0480@gmail.com>
2024-07-18 15:35:33 +0530

    Add files via upload

---
+++ newtest_project/src/newtest_project_packagefortesting/Sumnums.java
@@ -0,0 +1,15 @@
+package newtest_project_packagefortesting;
+
+public class Sumnums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int s= 70;
+		int g= 80;
+		int sum = s+g;
+		System.out.println(sum);
+
+	}
+
+}
